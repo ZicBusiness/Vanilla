e8b9244a5f36084f79bad48e72cff74c6cfb266e
ZicBusiness <77104285+ZicBusiness@users.noreply.github.com>
2022-11-27 14:52:00 -0300

    Change 01

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ playerImage.src = 'images/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 0;
+let frameY = 1;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
